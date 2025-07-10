cd63782dbf4e844ec063cd2dae3e176ddef96eac
Henrique Honorio da Silva <contato@henriquesilva.dev>
2025-07-10 20:40:57 -0300

    update painel

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -42,6 +42,7 @@ const Navbar: React.FC<NavbarProps> = ({ role }) => {
       { label: 'Dashboard', href: '/operador' },
       { label: 'Recepcionistas', href: '/operador/recepcionistas' },
       { label: 'Participantes', href: '/operador/participantes' }, // Este será um dropdown
+      { label: 'Painel de Recepção', href: '/operador/painel/{eventoId}' }, // Este será um dropdown
     ];
   } else if (role === 'recepcionista') {
     navItems = [
