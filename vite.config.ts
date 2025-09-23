1ff37ef3ced01862b90f4cad00a1147a877b2971
Henrique Honorio da Silva <contato@henriquesilva.dev>
2025-09-23 20:32:18 -0300

    fix: remove crypto.hash

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
-// vite.config.ts
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import { nodePolyfills } from "vite-plugin-node-polyfills"
 
 export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react']
+  plugins: [react(), nodePolyfills()],
+  resolve: {
+    alias: {
+      crypto: "crypto-browserify",
+    },
   },
-  define: {
-  }
-});
+})
